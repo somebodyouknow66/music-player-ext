22e87239b5e7addea3dc3b68e065f158e756d201
Sakhi Akram <sakhiakram.github@outlook.com>
2026-07-11 22:14:11 +0500

    offscreen.js error testing
    i had written above 50 lines in offscreen.js but the errors were to many and nothing was actually working so i just stripped it to the bare minimum

--- offscreen.js
+++ offscreen.js
@@ -1,1 +1,9 @@
-console.log("offscreen document is live")
+console.log("offscreen document is live");
+
+const TEST_VIDEO_ID = "NyscFyaSwg0";
+
+const iframe = document.createElement("iframe");
+iframe.width = "300";
+iframe.height = "200";
+iframe.src = `https://www.youtube.com/embed/${TEST_VIDEO_ID}`;
+document.getElementById("player").appendChild(iframe);
