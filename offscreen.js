a9da2d39b4a6618417d5c182429c17c96f5a95f1
Sakhi Akram <sakhiakram.github@outlook.com>
2026-07-12 12:43:56 +0500

    offscreen complete logic

--- offscreen.js
+++ offscreen.js
@@ -1,10 +1,41 @@
 console.log("offscreen document is live");
 
-const TEST_VIDEO_ID = "M7lc1UVf-VE";
+function ensurePlayer(trackUrl, autoplay) {
+    const src = "https://w.soundcloud.com/player/?url=" + encodeURIComponent(trackUrl) + "&auto_play=" + (autoplay ? "true" : "false");
+    if (!iframe) {
+        iframe = document.createElement("iframe");
+        iframe.width = "300";
+        iframe.height = "166";
+        iframe.allow = "autoplay";
+        iframe.src = src;
 
-const iframe = document.createElement("iframe");
-iframe.width = "300";
-iframe.height = "166";
-iframe.src = "https://w.soundcloud.com/player/?url=" + encodeURIComponent("https://soundcloud.com/forss/flickermood");
-document.getElementById("player").appendChild(iframe);
+        document.getElementById("player").appendChild(iframe);
+    } else {
+        iframe.src = src;
+    }
+}
 
+function sendToWidget(method) {
+    if (!iframe) return;
+    iframe.contentWindow.postMessage(JSON.stringify({ method }), "https://w.soundcloud.com")
+}
+
+chrome.runtime.onMessage.addListener((msg) => {
+    if (!msg || msg.target !== "offscreen") return;
+    switch (msg.type) {
+        case "LOAD_AND_PLAY": 
+        ensurePlayer(msg.trackUrl, true);
+        break;
+        case "PLAY": 
+        sendtoWidget("play");
+        break;
+        case "PAUSE": 
+        sendToWidget("pause");
+        break;
+    }
+});
+
+window.addEventListener("message", (e) => {
+    if (e.orgin !== "https://w.soundcloud.com") return;
+    console.log("from soundcloud:", e.data)
+})
